897e34b0fbe5b2cee5de62dacce10f4bde9ae512
YiYunLiao <kevin19951001@gmail.com>
2016-04-30 23:25:41 +0800

    update library

--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -20,8 +20,8 @@ public class MainApplet extends PApplet{
 	private final static int width = 1200, height = 650;
 	
 	public void setup() {
-
 		size(width, height);
+		Ani.init(this);
 		smooth();
 		loadData();
 		
