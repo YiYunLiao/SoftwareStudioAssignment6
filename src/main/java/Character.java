b5fa61320e57c17c56dc6b7b45a580f34724f9b9
YiYunLiao <kevin19951001@gmail.com>
2016-05-01 12:21:05 +0800

    update Character

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,6 +1,7 @@
 package main.java;
 
-import processing.core.PApplet;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
 * This class is used to store states of the characters in the program.
@@ -9,15 +10,63 @@ import processing.core.PApplet;
 public class Character {
 	
 	private MainApplet parent;
+	private String name, colour;
+	private float defaultX, defaultY, curX, curY, radius;
+	private Map<Character, Integer> targets;
 
-	public Character(MainApplet parent){
 
+	public Character(MainApplet parent, String name, String colour, float x, float y, float radius){
 		this.parent = parent;
-		
+		this.name = name;
+		this.colour = colour;
+		defaultX = x;
+		defaultY = y;
+		curX = x;
+		curY = y;
+		this.radius = radius;
+		targets = new HashMap<Character, Integer>();
 	}
 
 	public void display(){
+		parent.noStroke();
 
 	}
 	
+	
+	@SuppressWarnings("static-access")
+	public boolean arrowIsInCircle(){
+		float diffX = parent.mouseX - curX;
+		float diffY = parent.mouseY - curY;
+
+		if(parent.sqrt(parent.sq(diffX) + parent.sq(diffY)) < radius){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	
+	public void addTarget(Character target, int value){
+		targets.put(target, value);
+	}
+	
+	
+	public Map<Character, Integer> getTargets(){
+		return targets;
+	}
+	
+	
+	public String getName(){
+		return name;
+	}
+	
+	
+	public float getX(){
+		return curX;
+	}
+	
+	
+	public float getY(){
+		return curY;
+	}
 }
